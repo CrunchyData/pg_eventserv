af4fab4d97688d20dd802bf253fa9083bc11fde1
Paul Ramsey <pramsey@cleverelephant.ca>
2022-09-23 14:34:59 -0700

    add some comments

--- examples/chat/chat.js
+++ examples/chat/chat.js
@@ -2,6 +2,8 @@ var channelDefault = "public";
 var wsUrlBase = "ws://localhost:7700/listen/";
 var fsUrlBase = "http://localhost:9000";
 
+// Set up the defaults and hook up the events
+// once the page is finished loading.
 window.onload = function() {
   var userRandom = Math.floor(Math.random() * 1000);
   var userDefault = "User-" + userRandom;
@@ -13,12 +15,17 @@ window.onload = function() {
   wsConnect();
 }
 
+// Send the current message on enter as well
+// as on button click.
 function fsEnter(event) {
   if (event.key == "Enter") {
     fsSend();
   }
 }
 
+// When the channel connect button is clicked
+// (and at the end of the page load routine)
+// we connect to the event server.
 function wsConnect() {
   var channel = document.getElementById("channel").value;
   var wsUrl = wsUrlBase + channel;
@@ -49,12 +56,15 @@ function wsConnect() {
       return;
     }
 
+    // We can only process payloads that have an expected
+    // structure.
     if (!("user_name" in payload && "message" in payload)) {
       outputChat.innerHTML = "Error, unknown JSON object on channel:\n\n";
       outputChat.innerHTML += JSON.stringify(payload);
       return;
     }
 
+    // Append the message content to the chat window!
     var user_name = payload.user_name;
     var message = payload.message;
     var message = `[${user_name}] ${message}\n`;
@@ -63,8 +73,12 @@ function wsConnect() {
   };
 }
 
+// We send new message into the system via the pg_featureserv
+// function "message_send(username, channel, message)
 function fsSend() {
 
+  // Read the data from the form and encode it into
+  // a URL data object.
   var prms = new URLSearchParams({
     username: document.getElementById("username").value,
     channel: document.getElementById("channel").value,
@@ -76,10 +90,11 @@ function fsSend() {
   xmlhttp.onreadystatechange = function() {
     if (xmlhttp.readyState == XMLHttpRequest.DONE) { // XMLHttpRequest.DONE == 4
         if (xmlhttp.status == 200) {
-          // processed move
+          // processed action, so clear the current message from the input
           document.getElementById("message").value = "";
         }
         else {
+          // something bad happened, expose that to the user
           outputChat.innerHTML = `Error, send to ${fsUrlBase} failed.`;
       }
     }
--- examples/chat/chat.sql
+++ examples/chat/chat.sql
@@ -3,6 +3,8 @@ DROP SCHEMA IF EXISTS chat CASCADE;
 CREATE SCHEMA chat;
 SET search_path = chat,public;
 
+-- store the users and channels in normalized
+-- form like good little database designers!
 DROP TABLE IF EXISTS chat.users;
 CREATE TABLE chat.users (
   id SERIAL PRIMARY KEY,
@@ -11,6 +13,7 @@ CREATE TABLE chat.users (
   latest timestamptz NOT NULL DEFAULT Now()
 );
 
+-- we want unique user names!
 CREATE UNIQUE INDEX users_name_x
   ON chat.users(name);
 
@@ -22,9 +25,12 @@ CREATE TABLE chat.channels (
   latest timestamptz NOT NULL DEFAULT Now()
 );
 
+-- we want unique channel names!
 CREATE UNIQUE INDEX channels_name_x
   ON chat.channels(name);
 
+-- Since channel and user names are normalized
+-- we declare them as foreign key constraints
 DROP TABLE IF EXISTS chat.messages;
 CREATE TABLE chat.messages (
   id SERIAL PRIMARY KEY,
@@ -34,15 +40,23 @@ CREATE TABLE chat.messages (
   message TEXT
 );
 
+-- Future use: searching the contents of the
+-- chat messages using full text search!
 CREATE INDEX messages_message_tsx
   ON chat.messages
   USING GIST (to_tsvector('english', message));
 
+-- Might want to search using time stamp
+-- to get a particular slice of chats.
 CREATE INDEX messages_ts_x
   ON chat.messages (ts);
 
-
 CREATE SCHEMA IF NOT EXISTS postgisftw;
+
+-- We need one external function (at least) to
+-- send in new messages to the system. Other functions
+-- we might want to add could list the existing
+-- channel and users.
 CREATE OR REPLACE FUNCTION postgisftw.message_send(
 	username text, channel text, message text)
 RETURNS TABLE(user_id bigint, channel_id bigint, message_id bigint, ts timestamptz)
@@ -53,22 +67,29 @@ DECLARE
   message_id bigint;
 BEGIN
 
-  -- Normalize the user and channel names a bit
+  -- Normalize the user and channel names to lower case
   username := lower(username);
   channel := lower(channel);
 
+  -- Read the user id if it already exists, or add it
+  -- if it does not
   INSERT INTO chat.users (name) VALUES (username)
       ON CONFLICT (name) DO UPDATE SET latest = Now()
       RETURNING id INTO user_id;
 
+  -- Read the channel id if it already exists, or add it
+  -- if it does not
   INSERT INTO chat.channels (name) VALUES (channel)
       ON CONFLICT (name) DO UPDATE SET latest = Now()
       RETURNING id INTO channel_id;
 
+  -- Add the message into the message log table
   INSERT INTO chat.messages (user_id, channel_id, message)
     VALUES(user_id, channel_id, message)
     RETURNING id INTO message_id;
 
+  -- Return the ids to the client in case it wants
+  -- to maintain a local buffer of messages, etc.
   RETURN QUERY SELECT
     user_id AS user_id,
     channel_id AS channel_id,
@@ -79,7 +100,12 @@ END;
 $$
 LANGUAGE 'plpgsql' VOLATILE;
 
-
+-- When a new message hits the message log
+-- we want to trigger a message on the listen/notify
+-- bus so all the connected clients see the message.
+-- Mostly this is just bundling up everything
+-- we know into a JSON payload and sending it
+-- off over the bus.
 DROP FUNCTION IF EXISTS message_broadcast CASCADE;
 CREATE FUNCTION message_broadcast() RETURNS trigger AS $$
     DECLARE
@@ -113,8 +139,11 @@ CREATE FUNCTION message_broadcast() RETURNS trigger AS $$
     END;
 $$ LANGUAGE 'plpgsql';
 
+-- We'll only do broadcasts for new messages. If we had a UI
+-- that supported editing, we could also broadcast on update and
+-- delete.
 DROP TRIGGER IF EXISTS message_broadcast ON chat.messages;
 CREATE TRIGGER message_broadcast
-    AFTER INSERT OR UPDATE ON chat.messages
+    AFTER INSERT ON chat.messages
     FOR EACH ROW
         EXECUTE FUNCTION message_broadcast();
